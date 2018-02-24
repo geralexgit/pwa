d7950f41175a802de375447c109a006aa2f14df9
User <geralexndr@gmail.com>
2018-02-24 17:04:44 +0300

    service worker's events added

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', function (event) {
+    console.log('[Service Worker] Installing service worker', event);
+});
+
+self.addEventListener('activate', function (event) {
+    console.log('[Service Worker] Activating service worker', event);
+    return self.clients.claim();
+})
