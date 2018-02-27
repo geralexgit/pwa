ba3d7482a5c0610d366e82ec3a18d849c827b884
User <geralexndr@gmail.com>
2018-02-27 16:37:34 +0300

    files cahced, app available offline

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,21 @@ self.addEventListener('install', function (event) {
         caches.open('static')
         .then(function (cache) {
             console.log('[Service Worker] Precaching App Shell');
-            cache.add('/src/js/app.js');
+            cache.addAll([
+                '/',
+                '/index.html',
+                '/src/js/app.js',
+                '/src/js/feed.js',
+                '/src/js/promise.js',
+                '/src/js/fetch.js',
+                '/src/js/material.min.js',
+                '/src/css/app.css',
+                '/src/css/feed.css',
+                '/src/images/main-image.jpg',
+                'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                'https://fonts.googleapis.com/icon?family=Material+Icons',
+                'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+            ]);
         })
     );
 });
@@ -15,6 +29,14 @@ self.addEventListener('activate', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-    // console.log('[Service Worker] fetch triggered', event);
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+        .then(function (response) {
+            if (response) {
+                return response;
+            } else {
+                return fetch(event.request);
+            }
+        })
+    );
 });
